9adbd6d1acbc87dcb23020844afc9f763af1e90a
ShubhamIOsense <116338753+ShubhamIOsense@users.noreply.github.com>
2022-11-19 19:22:44 +0530

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherUrl: "https://weatherapi-com.p.rapidapi.com/current.json",
+
+  XRapidAPIHostHeaderName: "X-RapidAPI-Host",
+  XRapidAPIHostHeaderValue: "weatherapi-com.p.rapidapi.com",
+  XRapidAPIKeyHeaderName: "X-RapidAPI-Key",
+  XRapidAPIKeyheaderVlaue: "77243a639dmsh1d1715389c8f3c2p13bc84jsn8bf3117b06a4",
 };
